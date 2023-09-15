134c03a9d04e5ed5003b27400110bb2ee0e31ba9
Tembero11 <silver.teemu@gmail.com>
2023-09-16 00:36:45 +0300

    Add basic game server functionality

---
+++ src/game_server.rs
@@ -0,0 +1,53 @@
+use std::process::{Command, Stdio};
+use std::io::{BufReader, BufRead};
+
+#[derive(Clone, Debug)]
+pub struct GameServerCommand {
+  pub cmd: String,
+  pub args: Vec<String>,
+}
+impl GameServerCommand {
+  pub fn get_full_cmd(self) -> String {
+    format!("{} {}", self.cmd, self.args.join(" "))
+  }
+}
+
+#[derive(Clone, Debug)]
+pub struct GameServer {
+  pub name: String,
+  pub cmd: GameServerCommand,
+  // Null if not connected to database yet
+  pub id: Option<i32>
+}
+impl GameServer {
+    pub fn get_full_cmd(self) -> String {
+      self.cmd.get_full_cmd()
+    }
+}
+
+pub fn start_server(game_server: GameServer) {
+  let full_cmd_str = game_server.clone().get_full_cmd();
+  println!("{}", full_cmd_str);
+
+  run_cmd(game_server.cmd.cmd, game_server.cmd.args);
+}
+
+pub fn run_cmd(cmd_str: String, args: Vec<String>) {
+  let mut cmd = Command::new(cmd_str)
+        .args(args)
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Server failed to start!");
+
+    {
+        let stdout = cmd.stdout.as_mut().unwrap();
+        let stdout_reader = BufReader::new(stdout);
+        let stdout_lines = stdout_reader.lines();
+
+        for line in stdout_lines {
+            println!("Read: {:?}", line.unwrap());
+        }
+    }
+
+    cmd.wait().unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use crate::game_server::{GameServer, GameServerCommand};
+
+mod game_server;
+
 fn main() {
-    println!("Hello from Masa!");
+  println!("Hello from Masa!");
+
+  game_server::run_cmd(String::from("java"), Vec::from([String::from("--version")]));
+
+  let test_server = GameServer {
+    name: String::from("testi_servu"),
+    cmd: GameServerCommand {
+      cmd: String::from("java"),
+      args: Vec::from([String::from("--version")])
+    },
+    id: None,
+  };
+
+  game_server::start_server(test_server);
+  
 }
